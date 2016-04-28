a8ec8966beefd66529c8bd01fa2f472822cf9c79
Xiaojian Deng <xjd001@gmail.com>
2016-04-28 14:20:47 -0700

    refactor: Updated agent.py to use QLearner

--- smartcab/smartcab/agent_old.py
+++ smartcab/smartcab/agent_old.py
@@ -2,7 +2,6 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
-from QLearner import QLearner
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
@@ -15,21 +14,29 @@ class LearningAgent(Agent):
         self.discount_factor = 0.5
         self.learning_rate = 0.5
         self.default_Q = 2
-        self.Q0 = {}
         self.none_count = 0
         self.max_none = 6
+        self.state0 = None
+        self.action0 = None
+        self.reward0 = None
+        self.max_trials = 100
+        self.Q = {}
         for i in ['forward','left','right']:
             for j in ['green','red']:
                 for k in self.env.valid_actions:
-                    self.Q0[(i,j),k] = self.default_Q
-        self.Q = QLearner(self.Q0, self.env.valid_actions, self.discount_factor, self.learning_rate)
+                    self.Q[(i,j),k] = self.default_Q
+    
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
-        self.Q.reset()
+        if (self.state0, self.action0, self.reward0) != (None, None, None):
+            oldQ = self.Q[(self.state0,self.action0)]
+            self.Q[(self.state0,self.action0)] = oldQ + self.learning_rate*(self.reward0 - oldQ)
         self.none_count = 0
-
+        (self.state0, self.action0, self.reward0) = (None, None, None)
+        
+        
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
@@ -38,9 +45,18 @@ class LearningAgent(Agent):
 
         # TODO: Update state
         self.state = (self.next_waypoint, inputs['light'])
-        
         # TODO: Select action according to your policy
-        action = self.Q.pickAction(self.state)
+        newQ = -9999999999
+        action = None
+        
+        if self.Q[(self.state,None)] == self.Q[(self.state,'left')] == self.Q[(self.state,'right')] == self.Q[(self.state,'forward')]:
+            action = random.choice(Environment.valid_actions)
+            newQ = self.Q[(self.state,'left')]
+        else:      
+            for i2 in [None,'left','right','forward']:
+                if self.Q[(self.state,i2)] >= newQ:
+                    newQ = self.Q[(self.state,i2)]
+                    action = i2
         if action == None:
             self.none_count = self.none_count + 1
         else:
@@ -50,12 +66,13 @@ class LearningAgent(Agent):
 
         # Execute action and get reward
         reward = self.env.act(self, action)
-        
+
 
         # TODO: Learn policy based on state, action, reward
-
-        self.Q.updateState(self.state,reward,newAction = action)
-
+        if (self.state0, self.action0, self.reward0) != (None, None, None):
+            oldQ = self.Q[(self.state0,self.action0)]
+            self.Q[(self.state0,self.action0)] = (1-self.learning_rate)*oldQ + self.learning_rate*(self.reward0 + self.discount_factor*newQ)
+        (self.state0, self.action0, self.reward0) = (self.state, action, reward)
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
 
@@ -68,8 +85,9 @@ def run():
     e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=100)  # press Esc or close pygame window to quit
+    # sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
+    sim = Simulator(e, update_delay=1.0)
+    sim.run(n_trials=a.max_trials)  # press Esc or close pygame window to quit
 
 
 if __name__ == '__main__':
